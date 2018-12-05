67b372fd1f84ec8a37b593275890269a56382296
Antti Haapala <antti.haapala@anttipatterns.com>
2018-12-05 07:19:28 +0200

    solution for sneaky day 5

--- days/day05.py
+++ days/day05.py
@@ -1,11 +1,32 @@
+import string
+
 from helpers import *
 
 d = get_aoc_data(day=5)
 
 
+def react(polymer):
+    l = list(polymer)
+    i = 0
+    while True:
+        if l[i].swapcase() == l[i + 1]:
+            del l[i:i + 2]
+            if i > 0:
+                i -= 1
+        else:
+            i += 1
+        if i >= len(l) - 1:
+            break
+    return len(l)
+
+
 def part1():
-    ...
+    return react(d.data)
 
 
 def part2():
-    ...
+    lenghts = [
+        react(d.data.replace(i, '').replace(i.upper(), ''))
+        for i in string.ascii_lowercase
+    ]
+    return min(lenghts)
